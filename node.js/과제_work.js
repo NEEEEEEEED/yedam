b2a896814a725790644f17938eccf22af2c764e9
NEEEEEEEED <120690595+NEEEEEEEED@users.noreply.github.com>
2022-12-26 17:57:30 +0900

    20221226

---
+++ node.js/과제_work.js
@@ -0,0 +1,22 @@
+/*과제_work.js*/
+// import http from 'http'; 아래와 동일 결과, 방식다름
+const http = require("http");
+
+let todoList = [
+  { content: "test1", completed: false },
+  { content: "test2", completed: true },
+  { content: "test3", completed: false },
+  { content: "test4", completed: false },
+];
+
+const server = http.createServer((req, res) => {
+  const myURL = new URL("http://127.0.0.1:3000" + req.url);
+  if (myURL.pathname == "/todoList") {
+    res.end(JSON.stringify(todoList));
+  } else if (url.pathname == "/todo") {
+    res.end(myurl.searchParams.get(todoList[0]));
+  }
+});
+server.listen(3000, () => {
+  console.log("server running http://127.0.0.1:3000");
+});
