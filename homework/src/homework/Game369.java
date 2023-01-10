0999a22c389bf0cdc67b51a9f9a07b5e8d9b0605
NEEEEEEEED <120690595+NEEEEEEEED@users.noreply.github.com>
2023-01-10 14:30:24 +0900

    homework

---
+++ homework/src/homework/Game369.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class Game369 {
+
+	public static void main(String[] args) {
+		for(int i =0; i < 51; i++) {
+			if (i % 100 /10 == 3 || i % 100 /10 == 6 || i % 100/10 == 9 ) {
+				if(i % 100 %10 == 3 || i % 100 %10 == 6 || i % 100%10 == 9) {
+					
+				}
+			}
+			else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+				
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
