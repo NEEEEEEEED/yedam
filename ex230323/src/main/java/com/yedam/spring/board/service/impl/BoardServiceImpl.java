fc09e098013c2c2e2c7530a68c66f3b946b33e84
Hyosang Ryu <gytkd4313@naver.com>
2023-03-27 20:44:03 +0900

    2023.03.27home

--- ex230323/src/main/java/com/yedam/spring/board/service/impl/BoardServiceImpl.java
+++ ex230323/src/main/java/com/yedam/spring/board/service/impl/BoardServiceImpl.java
@@ -13,30 +13,53 @@ public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	BoardMapper boardMapper;
-	
+
 	@Override
-	public List<BoardVO> getBoardAll() {
+	public List<BoardVO> getBoardList() {
 		return boardMapper.selectBoardList();
 	}
 
 	@Override
-	public BoardVO getBoard(BoardVO boardVO) {
+	public BoardVO getBoardInfo(BoardVO boardVO) {
 		return boardMapper.selectBoardInfo(boardVO);
 	}
 
 	@Override
-	public int insertBoard(BoardVO boardVO) {
-		return boardMapper.insertBoard(boardVO);
+	public BoardVO getBoardNo() {
+		return boardMapper.getBoardNo();
 	}
 
 	@Override
-	public int updateBoard(BoardVO boardVO) {
-		return boardMapper.updateBoard(boardVO);
+	public int insertBoardInfo(BoardVO boardVO) {
+		BoardVO vo = new BoardVO();
+		vo.setBno(boardVO.getBno());
+		if(boardMapper.insertBoard(boardVO) ==1) {
+			return vo.getBno();
+		} else {
+			return -1;
+		}
+		
 	}
 
 	@Override
-	public int deleteBoard(int bno) {
-		return boardMapper.deleteBoard(bno);
+	public int updateBoardInfo(BoardVO boardVO) {
+		BoardVO vo = new BoardVO();
+		vo.setBno(boardVO.getBno());
+		if(boardMapper.updateBoard(boardVO) == 1) {
+			return vo.getBno();
+		} else {
+			return -1;
+		}
 	}
+
+	@Override
+	public int deleteBoardInfo(int bno) {
+		if(boardMapper.deleteBoard(bno) == 1) {
+			return bno;
+		} else {
+			return -1;
+		}
+	}
+	
 
 }
