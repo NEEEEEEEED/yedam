4aa5a33237ba9136ed72371fa233561b5fb14ddc
suna <xkq3355@naver.com>
2023-04-28 15:57:29 +0900

    2023-04-28 suna

---
+++ MoaMore/src/main/java/com/yedam/moa/MoaMoreApplication.java
@@ -0,0 +1,29 @@
+package com.yedam.moa;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@MapperScan(basePackages="com.yedam.moa.**.mapper")
+@Controller
+public class MoaMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaMoreApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "MoaMore";
+	}
+	
+
+}
