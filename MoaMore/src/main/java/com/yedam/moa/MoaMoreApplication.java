4d3b11bf0eda5583741fa988c68fce17002658c6
Hyosang Ryu <gytkd4313@naver.com>
2023-05-02 00:28:00 +0900

    2023.05.02

--- MoaMore/src/main/java/com/yedam/moa/MoaMoreApplication.java
+++ MoaMore/src/main/java/com/yedam/moa/MoaMoreApplication.java
@@ -3,6 +3,8 @@ package com.yedam.moa;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,19 +12,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @MapperScan(basePackages="com.yedam.moa.**.mapper")
 @Controller
 public class MoaMoreApplication {
+	//비밀번호 보안
+	@Bean
+	public BCryptPasswordEncoder encoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoaMoreApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
+	@GetMapping("/login")
 	public String hello() {
-		return "hello";
+		return "login";
 	}
+	
+	/*
+	 * @GetMapping("/") public String home() { return "MoaMore"; }
+	 */
 	
 	@GetMapping("/")
 	public String home() {
-		return "MoaMore";
+		return "login";
 	}
 	
 
---
+++ MoaMore/src/main/java/com/yedam/moa/member/service/MemberService.java
@@ -0,0 +1,6 @@
+package com.yedam.moa.member.service;
+
+public interface MemberService {
+
+	MemberVO getMember(String id);
+}
