963409cb069d60ba2984322d7dee3b1c4890b67f
'uk' <'dnrdl6817@gamil.com'>
2023-05-02 17:52:30 +0900

    야발

---
+++ MoaMore/src/main/java/com/yedam/moa/mem/mapper/MemMapper.java
@@ -0,0 +1,5 @@
+package com.yedam.moa.mem.mapper;
+
+public interface MemMapper {
+
+}
---
+++ MoaMore/src/main/java/com/yedam/moa/mem/service/Impl/MemServiceImpl.java
@@ -0,0 +1,10 @@
+package com.yedam.moa.mem.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yedam.moa.mem.service.MemService;
+
+@Service
+public class MemServiceImpl implements MemService {
+
+}
