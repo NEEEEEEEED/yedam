cbb562af7d94a9f6e77831671673bd7f34b41316
NEEEEEEEED <120690595+NEEEEEEEED@users.noreply.github.com>
2023-05-02 17:01:21 +0900

    2023.05.02 류효상

---
+++ MoaMore/src/main/java/com/yedam/moa/admin/service/AdminService.java
@@ -0,0 +1,5 @@
+package com.yedam.moa.admin.service;
+
+public interface AdminService {
+
+}
---
+++ MoaMore/src/main/java/com/yedam/moa/admin/service/impl/AdminServiceImpl.java
@@ -0,0 +1,10 @@
+package com.yedam.moa.admin.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yedam.moa.admin.service.AdminService;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+}
