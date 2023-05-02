01314b93ef1a78d79f478eb111651da1e9c01414
uk <dnrdl6817@naver.com>
2023-05-03 00:44:12 +0900

    20230502 commithome

--- MoaMore/src/main/java/com/yedam/moa/mem/web/MemController.java
+++ MoaMore/src/main/java/com/yedam/moa/mem/web/MemController.java
@@ -1,5 +1,7 @@
 package com.yedam.moa.mem.web;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,15 @@ import com.yedam.moa.mem.service.MemService;
 
 @Controller
 public class MemController {
-	
+
+
 	@Autowired
 	MemService mem;
 	
+	
 	@GetMapping("mem/mypage")
-	public String getMyPgae() {
+	public String getMyPgae(Principal principal) {
+		System.out.println(principal.getName());
 		return "mem/mem_mypage";
 	}
 }
