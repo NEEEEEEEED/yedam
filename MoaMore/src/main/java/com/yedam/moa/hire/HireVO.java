dd1944847930ecaa70218998b2a48172d1a13b5c
SuCock <ksymin313@gamil.com>
2023-05-02 17:50:11 +0900

    23.05.02 5:49pm soomin

--- MoaMore/src/main/java/com/yedam/moa/hire/HireVO.java
+++ MoaMore/src/main/java/com/yedam/moa/hire/HireVO.java
@@ -4,5 +4,23 @@ import lombok.Data;
 
 @Data
 public class HireVO {
-
+	String recruitNo; // 구인공고 번호
+	String id; // 회원아이디
+	String ttl; // 구인공고 제목
+	String skill; // 기술스펙
+	String shcr; // 학력
+	String carr; // 경력
+	String maBuss; // 주요업무
+	String qualCond; // 자격요건
+	String prefCond; // 우대사항
+	String welf; // 복지 및 혜택
+	String exprDt; // 마갈일
+	String wksite; // 근무지역
+	String coIntro; // 기업 소개
+	String coImg; // 기업 이미지
+	String accpDt; // 승인날짜
+	String imgNo; // 이미지 그룹번호
+	
+	String coNm; // 기업이름
+	String addr; // 기업주소
 }
