18cabbb7bf080aee6e0736f9532f72f94366dd11
NEEEEEEEED <120690595+NEEEEEEEED@users.noreply.github.com>
2023-05-03 17:17:50 +0900

    2023.05.03 류효상

--- MoaMore/src/main/java/com/yedam/moa/admin/web/AdminController.java
+++ MoaMore/src/main/java/com/yedam/moa/admin/web/AdminController.java
@@ -1,10 +1,14 @@
 package com.yedam.moa.admin.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yedam.moa.admin.service.AdminService;
+import com.yedam.moa.mem.MemVO;
 
 @Controller
 public class AdminController {
@@ -19,6 +23,12 @@ public class AdminController {
 	  @GetMapping("/member/common") 
 	  public String commonMember() { 
 		  return "admin/commonMember"; 
+	  }
+	  
+	  @GetMapping("/getUserInfo") 
+	  @ResponseBody
+	  public List<MemVO> getUserInfo() {
+		  return adminService.getUserInfo(); 
 	  }
 	 
 	
--- MoaMore/src/main/java/com/yedam/moa/mem/MemVO.java
+++ MoaMore/src/main/java/com/yedam/moa/mem/MemVO.java
@@ -4,5 +4,22 @@ import lombok.Data;
 
 @Data
 public class MemVO {
-
+	private String id;		//아이디
+	private String name;	//이름
+	private String pw;		//비밀번호
+	private String ph;		//전화번호
+	private String phCl;	//휴대폰
+	private String email;	//이메일
+	private String blog;	//개인홈페이지
+	private String post;	//우편번호
+	private String authr;	//권한
+	private String clsf;	//회원분류
+	private String kakao;	//카카오
+	private String naver;	//네이버
+	private String logCnt;	//로그인횟수
+	private String ltstTm;	//최근로그인시간
+	private String gen;		//성별
+	private String birth;	//생년월일
+	private String addr;	//주소
+	private String joinDt;	//가입일자
 }
