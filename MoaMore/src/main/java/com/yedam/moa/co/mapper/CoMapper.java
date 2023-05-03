c84267f068f9430cd758d10a31a491e4eb1d1706
goodbbak <wndks8564@naver.com>
2023-05-03 17:18:49 +0900

    05-03 17:18

---
+++ MoaMore/src/main/java/com/yedam/moa/co/mapper/CoMapper.java
@@ -0,0 +1,20 @@
+package com.yedam.moa.co.mapper;
+
+import java.util.List;
+
+import com.yedam.moa.co.service.CoVO;
+import com.yedam.moa.hire.HireVO;
+
+public interface CoMapper {
+	//기업정보 조회
+	public CoVO selectCo (CoVO vo);
+	
+	//기업정보 등록
+	public int insertCo(CoVO vo);
+	
+	//기업정보 수정
+	public int updateCo(CoVO vo);
+	
+	//구인공고목록
+	public List<HireVO> selectRec(HireVO vo);
+}
--- MoaMore/src/main/java/com/yedam/moa/co/web/CoMypageController.java
+++ MoaMore/src/main/java/com/yedam/moa/co/web/CoMypageController.java
@@ -1,21 +1,44 @@
 package com.yedam.moa.co.web;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.yedam.moa.co.service.CoService;
+import com.yedam.moa.co.service.CoVO;
+import com.yedam.moa.hire.HireVO;
+
 
 
 @Controller
 public class CoMypageController {
 		@Autowired
+		CoService service;
 		
+		//기업마이페이지
 		@GetMapping("/coMypage")
-		 public String coMypage(){ 
+		 public String coMypage(Principal principal,Model model){ 
+		 model.addAttribute("id", principal.getName());	//user id 가져오기
 		 return "co/coMypage"; 
 		 }
 		
-		@GetMapping("/coInfoPage")
-		  public String coInfoPage() {
+		//기업정보등록페이지
+		@GetMapping("/coInfoPage") //기업아이디 들고 들어가서 해당 아이디에 해당하는 기업정보 있다면 model에 service담아서 미리출력 
+		  public String coInfoPage(Model model, Principal principal, CoVO vo) { 
+			vo.setId(principal.getName()); 
+			model.addAttribute("co",service.selectCo(vo));
 			return "co/coInfo";
 		}
+		
+		//구인공고목록
+		@GetMapping("/coRecruit")
+		  public String coRecruitPage(Model model, Principal principal, HireVO vo) {
+			vo.setId(principal.getName()); 
+			model.addAttribute("rec",service.selectRec(vo));
+			return "co/coRecruit";
+		}
+		
 }
