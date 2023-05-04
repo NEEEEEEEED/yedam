1d29df523d282c4e6f27567fc5c9bebb31c3d8fd
NEEEEEEEED <120690595+NEEEEEEEED@users.noreply.github.com>
2023-05-04 16:58:04 +0900

    2023.05.04 류효상

---
+++ MoaMore/src/main/java/com/yedam/moa/admin/service/PostListVO.java
@@ -0,0 +1,11 @@
+package com.yedam.moa.admin.service;
+
+import lombok.Data;
+
+@Data
+public class PostListVO {
+	private String regDt;
+	private String postType;
+	private String ttl;
+	private String inqCnt;
+}
