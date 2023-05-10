024e3c70b8ba8f7e3919897ddf5a9387bb321670
goodbbak <wndks8564@naver.com>
2023-05-10 17:03:06 +0900

    05-10 17:02 juan

--- MoaMore/src/main/java/com/yedam/moa/co/service/CoVO.java
+++ MoaMore/src/main/java/com/yedam/moa/co/service/CoVO.java
@@ -17,4 +17,21 @@ public class CoVO {
 	private String homePage; //홈페이지
 	private String bizno; //사업자등록번호
 	private String coNm; //기업명
+	
+	//셀프구직 제안 양식
+	private String offerCd; //채용제안코드
+	private String wksite; //근무지
+	private String tech; //기술스택
+	private String maBuss; //주요업무
+	private String bnft; //복지 및 혜택
+	private String saly; //제안급여,연봉
+	private String bussStartDt; //업무시작일
+	private String clsDt; //제안 마감일
+	private String projectNm; //프로젝트명
+	private String ttl; //구인공고 
+	
+	//채용제안상태
+	private String jobSearchNo; //셀프구직게시글번호
+	private String acceptStatus; //제안수락상태
+	
 }
