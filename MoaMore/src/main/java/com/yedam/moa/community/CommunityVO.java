6c4174065bebda34d2b54f931e8562088189c41c
suna <xkq3355@naver.com>
2023-05-11 17:56:20 +0900

    2023_05_11 suna

--- MoaMore/src/main/java/com/yedam/moa/community/CommunityVO.java
+++ MoaMore/src/main/java/com/yedam/moa/community/CommunityVO.java
@@ -5,12 +5,9 @@ import lombok.Data;
 @Data
 public class CommunityVO {
 
-		private String id;
-		private String writer;
-		private String ctnt;
-		private String wdate;
-		byte[] imgData; // blob 이미지
-		private String atachFile;
-		private String atachType;
+	private String title;
+	private String content;
+	
+	
 
 }
--- MoaMore/src/main/java/com/yedam/moa/community/web/CommunityController.java
+++ MoaMore/src/main/java/com/yedam/moa/community/web/CommunityController.java
@@ -2,6 +2,7 @@ package com.yedam.moa.community.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -9,12 +10,14 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.yedam.moa.community.CommunityVO;
 import com.yedam.moa.community.service.Impl.CommunityServiceImpl;
 
 @Controller
@@ -26,6 +29,8 @@ public class CommunityController {
 	@Autowired 
 	CommunityServiceImpl communityServiceImpl; 
 	
+	Principal pr;
+	
 	// 취업 Q&A 페이지
 	@GetMapping("/jobQnA") 
 	public String jobQnaList() { 
@@ -34,11 +39,13 @@ public class CommunityController {
 	
 	// 취업 Q&A 게시글작성 페이지
 	@GetMapping("/jobQnAWrite")
-	public String jobQnAWrite() {
+	public String jobQnAWrite(Model model, Principal principal) {
+		pr = principal;
+		model.addAttribute("logId",pr.getName());
 		return "community/jobQnAWriteForm";
 	}
 	
-	// 취업Q&A Ckeditor 이미지 등록처리
+	// 취업Q&A Ckeditor 이미지를 올렸을때 처리
 	@PostMapping("/jobQnAImg")
 	@ResponseBody
 	public Map<String,Object> imgUpload(@RequestParam("upload") MultipartFile uploadFile) throws IllegalStateException, IOException{
@@ -50,24 +57,42 @@ public class CommunityController {
 		MultipartFile upload = uploadFile;
 		
 		String fileName = null; 		// 원본파일명
-		//String uploadFileName = null;	// UUID적용한 파일명
+		String uploadFileName = null;	// UUID적용한 파일명
 		
 		if(upload !=null && !upload.isEmpty() && upload.getSize()>0) {
 			
 			fileName = upload.getOriginalFilename(); // 원본파일명
 			
 			// 동일 파일명 처리 UUID 사용
-			//UUID uuid = UUID.randomUUID();
-			//uploadFileName = uuid.toString() + "_" + fileName;
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + fileName;
 			
-			upload.transferTo(new File("c:/moaImg/"+fileName)); // 파일업로드
+			upload.transferTo(new File("c:/moaImg/"+uploadFileName)); // 파일업로드
 			data.put("uploaded","1");
 			data.put("fileName",fileName);
-			data.put("url", "/moamoreImg/"+fileName);
+			data.put("UUIDFileName",uploadFileName);
+			data.put("url", "/moamoreImg/"+uploadFileName);
 			
 		}
-		
 		return data;
+	}
+	
+	// 취업 Q&A
+	@PostMapping("/jobQnaAdd")
+	@ResponseBody
+	public String jobQnaInsert(CommunityVO communityVO) {
+		int r = 0;
+		
+		System.out.println("title : " + communityVO.getTitle());
+		System.out.println("content :" + communityVO.getContent());
+		
+		
+		if(r > 0) {
+			return "{\"result\": \"Success\"}";
+		}else {
+			return "{\"result\": \"Fail\"}";
+		}
+		
 	}
 	
 	
